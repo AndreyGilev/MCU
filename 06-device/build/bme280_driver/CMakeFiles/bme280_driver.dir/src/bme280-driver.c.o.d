8fa25dc90172662e5291701428fe321861b48886
Гилёв Андрей <andreygil0808@gmail.com>
2026-03-27 17:46:41 +0300

    dir build added

---
+++ 06-device/build/bme280_driver/CMakeFiles/bme280_driver.dir/src/bme280-driver.c.o.d
@@ -0,0 +1,19 @@
+bme280_driver/CMakeFiles/bme280_driver.dir/src/bme280-driver.c.o: \
+ /home/andrew0808/Repositories/pico/MCU/libs/bme280-driver/src/bme280-driver.c \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/_locale.h \
+ /usr/include/newlib/strings.h /usr/include/newlib/sys/string.h \
+ /home/andrew0808/Repositories/pico/MCU/libs/bme280-driver/src/../include/bme280-driver.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /home/andrew0808/Repositories/pico/MCU/libs/bme280-driver/src/../include/bme280-regs.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/stdio.h
